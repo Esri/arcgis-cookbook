af000594b55a573c7fe56467307d432d42222f91
pave0000 <pbobov@esri.com>
2015-06-12 09:22:34 -0700

    Added source_url to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,12 @@
 name             'arcgis'
-maintainer       'Environmental Systems Research Institute, Inc.'
+maintainer       'Esri'
 maintainer_email 'contracts@esri.com'
 license          'Apache 2.0'
-description      'Installs and configures ArcGIS '
+description      'Installs and configures ArcGIS'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.1.0'
+issues_url       'https://github.com/ArcGIS/arcgis-cookbook/issues'
+source_url       'https://github.com/ArcGIS/arcgis-cookbook'
 
 depends          'hostsfile'
 depends          'limits'
