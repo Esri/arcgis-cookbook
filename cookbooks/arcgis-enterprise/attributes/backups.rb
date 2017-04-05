e5e9cf009decb93601542228d5d5e73f6bed2d24
Mirko Ulfig <mirko.ulfig@zuerich.ch>
2017-04-05 09:18:59 +0200

    Move resolution of Python's Runtime Environment Path into Misc

--- cookbooks/arcgis-enterprise/attributes/backups.rb
+++ cookbooks/arcgis-enterprise/attributes/backups.rb
@@ -8,14 +8,3 @@ default['arcgis']['backups'].tap do |backups|
   backups['webgis_backup_configuration_file'] = ::File.join(node['arcgis']['portal']['install_dir'], "tools", "webgisdr", "Custom_webgisdr.properties")
   backups['retention_period'] = 30
 end
-
-case node['arcgis']['version']
-when '10.5'
-  default['arcgis']['python']['python_runtime_environment'] = File.join(node['arcgis']['python']['install_dir'], "ArcGISx6410.5")
-when '10.4.1'
-  default['arcgis']['python']['python_runtime_environment'] = File.join(node['arcgis']['python']['install_dir'], "ArcGISx6410.4")
-when '10.4'
-  default['arcgis']['python']['python_runtime_environment'] = File.join(node['arcgis']['python']['install_dir'], "ArcGISx6410.4")
-else
-  throw 'Unsupported ArcGIS version'
-end
